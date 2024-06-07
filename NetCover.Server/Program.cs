074909bb5bacc23250a065aedfaa88fcf582e4c7
manasseh-zw <manassehtchanga@gmail.com>
2024-06-07 22:45:19 +0200

    fix: cloud

--- NetCover.Server/Program.cs
+++ NetCover.Server/Program.cs
@@ -16,11 +16,9 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 
 
